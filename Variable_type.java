d483c86fd49bfeb4b9ad83c9a9d351bc019b35f2
Allan Oliveira <allanolme@gmail.com>
2023-11-12 20:37:51 -0300

    Create Variable_type.java

---
+++ Variable_type.java
@@ -0,0 +1,7 @@
+public class Variable_type{
+    public static void main(String[] args) throws Exception{
+        String BR = "Imutável";
+        BR = "Imutável2";
+        System.out.println(BR);
+    };
+}
