ff9c7331539aa40a9af6e1a9f1fdf1932202fb4d
Allan Oliveira <allanolme@gmail.com>
2023-11-12 20:51:43 -0300

    Delete Variable_type.java

--- Variable_type.java
+++
@@ -1,7 +0,0 @@
-public class Variable_type{
-    public static void main(String[] args) throws Exception{
-        String BR = "Imutável";
-        BR = "Imutável2";
-        System.out.println(BR);
-    };
-}
